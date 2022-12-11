df2b898db1a9cbeb804eb4336fd7078230986b35
ADMIN <iryna.blazhenko@gmail.com>
2022-12-11 16:21:20 +0200

    13 zip

--- lesson9_assertions_built-in functions/hw13_zip1012.py
+++ lesson9_assertions_built-in functions/hw13_zip1012.py
@@ -17,67 +17,41 @@ def custom_zip(*sequences: Iterable, full=False, default=None) -> List[Tuple]:
 # -------------------------------------------------------------------------------------------------------------
 from typing import Iterable, List, Tuple
 
-
 seq1 = [1, 2, 3, 4, 5]
 seq2 = [9, 4, 2, 0]
 seq3 = [9, 4, 2, 6, 7, 3, 5]
 
 
-
 def custom_zip(*sequences: Iterable, full=False, default=None) -> List[Tuple]:
-    min_len = min(map(len, sequences))
-    print(min_len)
-    result_tuple = []
+    print(f'Последовательности: {sequences}')
+    len_sequences = len(sequences)  # Количество последовательностей
     i = 0
     j = 0
-    summa = ()
-
-    # result_tuple = sequences
-    # print(f'result_tuple[0] {result_tuple[0]}')
-    # a = ()
-    # b = []
-    # for i in range(min_len):
-    #     a = result_tuple[0][i] + result_tuple[1][i]
-    #     b.append(a)
-    #     print(f'a {a}')
-    # print(f'b {b}')
-
-    # result_tuple = sequences
-    # print(f'result_tuple[0] {result_tuple[0]}')
-    # a = ()
-    # b =[]
-    # for i in range(min_len):
-    #     a=0
-    #     for j in range(2):
-    #         a += result_tuple[j][i]
-    #     b.append(a)
-    #     print(f'a {a}')
-    # print(f'b {b}')
-
-
-    print(f'sequences {sequences}')
-    lisst = list(map(tuple,sequences))
-    print(f'sequences {lisst}')
-    length = len(sequences)
-    min_len = min(map(len, sequences))
-    print(min_len)
-    print(f'sequences {sequences}')
-    i = 0
-    a = tuple()
-    b = tuple()
-
-    for i in sequences:
-        print(f'i {i}')
-        for j, value in enumerate(i):
-            a = sum(sequences[][j])
-
-            print(f'a {a}')
-
-
-
+    items = []
+    result_tuple = []
+    if full:
+        max_len = max(map(len, sequences))  # максимальная длина последовательности
+        for i in range(len_sequences):
+            while len(sequences[i]) < max_len:
+                # расширение последовательностей до максимальной длины символами из default
+                sequences[i].append(default)
+        length = max_len
+    else:
+        length = min(list(map(len, sequences)))  # минимальная длина последовательности
+    for i in range(length):
+        for j in sequences:
+            items.append(j[i])
+        result_tuple.append(tuple(items[len(items) - len_sequences:]))
 
     return result_tuple
 
+
 zip_value = custom_zip(seq1, seq2, seq3)
-# print(zip_value)
+print(f'Результат выполнения custom_zip: \n\t{zip_value}')
 print(f'zip {list(zip(seq1, seq2, seq3))}')
+assert zip_value == list(zip(seq1, seq2, seq3))
+
+zip_value_full = custom_zip(seq1, seq2, seq3, full=True, default=None)
+print(f'Результат выполнения custom_zip с full=True: \n\t{zip_value_full}')
+print(f'zip {list(zip(seq1, seq2, seq3))}')
+assert zip_value_full == list(zip(seq1, seq2, seq3))
